2b25175723fed9f36d0631c131e06417d454a9ad
rohitmane9022 <rohitmane8356@gmail.com>
2026-02-10 20:53:37 +0530

    Fix SmallHeader for all device

--- components/SmallHeader.jsx
+++ components/SmallHeader.jsx
@@ -1,21 +1,40 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 const SmallHeader = () => {
+  const [duration, setDuration] = useState(20);
+
+  useEffect(() => {
+   
+    const handleResize = () => {
+      const width = window.innerWidth;
+      
+      const calculatedDuration = width / 80; 
+      setDuration(Math.max(12, Math.min(calculatedDuration, 30)));
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className="bg-black text-white relative z-50 h-10 flex items-center overflow-hidden">
+    <div className="bg-black text-white relative z-50 h-10 flex items-center overflow-hidden border-b border-zinc-800">
       <motion.div
-        className="whitespace-nowrap px-6 lg:w-full w-50 font-medium text-base"
-        initial={{ x: '140%' }}
-        animate={{ x: '-100%' }}
+        className="whitespace-nowrap font-medium text-sm md:text-base inline-block"
+        initial={{ x: "100vw" }} 
+        animate={{ x: "-100%" }} 
         transition={{
-          duration: 30,
+          duration: duration,
           ease: 'linear',
           repeat: Infinity,
         }}
       >
-        A Community First Hyperlocal Task Infrastructure |  SAYZO is not just a gig app | It is infrastructure for the neighbourhood economy
+        <span className="px-10">
+          A Community First Hyperlocal Task Infrastructure | SAYZO is not just a gig app | It is infrastructure for the neighbourhood economy
+        </span>
       </motion.div>
     </div>
   );
