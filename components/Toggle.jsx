613566a42a172d6475a61c495b057ca964567e10
mayanksaini18 <mayanksaini0416@gmail.com>
2026-03-19 12:35:35 +0530

    feat:add router

--- components/Toggle.jsx
+++ components/Toggle.jsx
@@ -1,31 +1,48 @@
 "use client";
-import { useState } from "react";
+
+import { useRole } from "@/context/RoleContext";
+import { useRouter , usePathname } from "next/navigation";
 
 export default function Toggle() {
-  const [viewMode, setViewMode] = useState("giver");
+  const router = useRouter();
+  const pathname = usePathname();
+  const { role, setRole } = useRole();
+  const isGiver = role === "giver";
 
-  const toggleMode = () => {
-    setViewMode((prev) => (prev === "giver" ? "doer" : "giver"));
-  };
+    const handleToggle = () => {
+        const newRole = isGiver ? "doer" : "giver";
+        setRole(newRole);
+
+        //check if user is on task-related pages
+        if(
+            pathname.includes("/dashboard/my-tasks")
+           || pathname.includes("/dashboard/applied-tasks") 
+        )
+        if( newRole ==="giver"){
+            router.push("/dashboard/my-tasks");
+        }
+        else{
+            router.push("/dashboard/applied-tasks");
+        }
+    }
 
   return (
     <button
-      onClick={toggleMode}
+      onClick={handleToggle}
+      aria-label={`Switch to ${isGiver ? "Doer" : "Giver"} mode`}
       className={`relative w-[72px] h-7 rounded-full border transition-colors duration-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:ring-gray-400 ${
-        viewMode === "giver"
-          ? "bg-white border-gray-300"
-          : "bg-black border-gray-900"
+        isGiver ? "bg-white border-gray-300" : "bg-black border-gray-900"
       }`}
     >
-      {/* Sliding Knob */}
+      {/* Sliding knob */}
       <div
         className={`absolute top-[3px] left-[3px] w-10 h-5 rounded-full shadow-md flex items-center justify-center text-[11px] font-semibold transition-transform duration-300 ease-out tracking-wide ${
-          viewMode === "giver"
+          isGiver
             ? "translate-x-0 bg-[#10A37F] text-white"
             : "translate-x-6 bg-white text-gray-900"
         }`}
       >
-        {viewMode === "giver" ? "Giver" : "Doer"}
+        {isGiver ? "Giver" : "Doer"}
       </div>
     </button>
   );
